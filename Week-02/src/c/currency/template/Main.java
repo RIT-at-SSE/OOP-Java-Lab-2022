4fed7887b26e8e68fcb8a91814c9782c9c9f0761
Paul Spiesberger <paul.spiesberger@rise-world.com>
2022-12-07 09:26:56 +0100

    Minor Code Improvements

--- Week-02/src/c/currency/template/Main.java
+++ Week-02/src/c/currency/template/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
         int firstCurrency, secondCurrency, keepRunning, amount;
--- Week-02/src/c/currency/template/Rupee.java
+++ Week-02/src/c/currency/template/Rupee.java
@@ -3,7 +3,7 @@ package c.currency.template;
 public class Rupee {
     // TODO: implement the ICurrency Interface and implement the functions
 
-    // Claculations:
+    // Calculations:
     // Rupee toEuro inr = 0.012 * eur
     // Rupee toDollar inr = 0.012 * usd
     // Rupee toYen inr = 1.66 * yen
